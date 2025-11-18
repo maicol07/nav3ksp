4d2641cf1ad393c2d29654cf797b1128a6c495b3
Ilya Dobryakov <ilya.dobryakov@icloud.com>
2025-11-19 01:28:03 +0300

    ci fix 3
    Took 3 minutes

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,6 @@ plugins {
 allprojects {
     group = "io.github.fopwoc.nav3ksp"
     version = providers.exec {
-        commandLine("git", "describe", "--tags", "--abbrev=0")
+        commandLine("git", "describe", "--tags", "--abbrev=0",  "--always")
     }.standardOutput.asText.get().trim()
 }
